44f1e309ce32bf1ff4aae6b7f8f6957866f65450
raff <raffaelelugiebllo@gmail.com>
2025-06-05 13:53:49 +0200

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-
+import { Button } from './components/ui/button'
 import './App.css'
 //test branch
 function App() {
@@ -10,6 +10,8 @@ function App() {
       <div className='flex flex-col items-center justify-center min-h-screen bg-black text-white'>
         <h1 className='text-4xl font-bold mb-4'>Vite + React + Tailwind CSS</h1>
         <p></p>
+        <Button>Hello</Button>
+         
       </div>
     </>
   )
