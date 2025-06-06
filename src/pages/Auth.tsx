a3b4022235780a83dad1468e915761bc2effbad1
raff <raffaelelugiebllo@gmail.com>
2025-06-06 13:05:19 +0200

    Auth parte grafica e inizio supabase

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
+import { LoginForm } from "@/components/login-form"
 
 const Auth = () => {
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center bg-testing text-white gap-[12px]">
-        <h1>Auth</h1>
+        
+         <LoginForm className="w-[400px]"/>
+        
         <Link to="/">
             <Button>Vai a home</Button>
         </Link>
