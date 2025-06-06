3c080e1c6f0754760a7d4a9506459a88d849fa6f
raff <raffaelelugiebllo@gmail.com>
2025-06-06 12:12:46 +0200

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-
-import { Button } from './components/ui/button'
-import Toast from './components/Elementi/Toast'
 import './App.css'
-//test branch
-function App() {
-  
+import Toast from './components/Elementi/Toast'
 
+//test branch
+  function App() {
+  
   return (
     <>
+    
       <div className='flex flex-col items-center justify-center min-h-screen bg-testing text-white gap-[12px]'>
         <h1 className='text-4xl font-bold mb-4'>Template Raffaele Lugibello </h1>
         <p> Il template comprende React - Vite con:</p>
-        <p> Tailwind, Shadcn, Supabase, tspattern, </p>
-        <Button>Test</Button>
-        <Toast></Toast>
-         
+        <p> Tailwind, Shadcn, Supabase, tspattern, React-hot-toast </p>
+        <Toast message='Exemple' color='#F00000'></Toast> 
       </div>
     </>
   )
--- src/components/Elementi/Toast.tsx
+++ src/components/Elementi/Toast.tsx
@@ -1,21 +1,31 @@
 import {Toaster, toast} from 'react-hot-toast';
 import { Button } from '../ui/button';
 
-function Toast() {
+interface ToastProps {
+    message?: string;
+    color?: string;
+}
+
+function Toast(toastProps: ToastProps) {
+    const { message } = toastProps || 'Default toast message';
+    const color = toastProps.color || 'bg-red-700';
+    
     return (
         <>
-        <Button className='bg-red-700' onClick={()=> toast("Exemple")}> Toast </Button>
+        
+        <Button className='bg-red-700' onClick={()=> toast(message!)}> Toast </Button>
         <Toaster 
         position='top-right' 
         toastOptions={{
         duration: 4000,
         style: {
-            background: '#333',
+            background: color,
             color: '#fff',
         },
         }}
         />
-
+        
+        
         </>
     )
 }
