129556a4d56144a8a755980c1ae4f60caeed338a
Raffaele Lugibello <raffaelelugibello@gmail.com>
2025-07-24 02:56:51 +0200

    fix

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,53 @@
-import * as React from "react"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+const inputVariants = cva(
+  "flex w-full min-w-0 px-3 py-2 border rounded-md bg-input text-foreground placeholder:text-muted-foreground transition-[color,box-shadow] shadow-xs outline-none focus-visible:ring-ring/50 focus-visible:ring-[3px] focus-visible:border-ring disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        primary: "border-input bg-white text-foreground",
+        email:
+          "border-input text-[14px] text-primary bg-white text-foreground focus-visible:border-blue-500 focus-visible:ring-blue-500/50",
+        password:
+          "border-input text-[14px] bg-input text-foreground tracking-wide focus-visible:border-red-500 focus-visible:ring-red-500/50",
+        deactive: "border-muted bg-muted text-muted-foreground",
+      },
+      size: {
+        default: "h-9 ",
+        sm: "h-8 text-sm",
+        lg: "h-10 text-lg",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+    },
+  }
+);
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+type NativeInputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "size"
+>;
 
-export { Input }
+export interface InputProps
+  extends NativeInputProps,
+    VariantProps<typeof inputVariants> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={cn(inputVariants({ variant, size }), className)}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input, inputVariants };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,22 @@
-import { useProfile } from "@/hooks/useProfile"
-import { Button } from "@/components/ui/button"
-import { AppSidebar } from "@/components/reusable/AppSidebar"
+import { useProfile } from "@/hooks/useProfile";
+import { Button } from "@/components/ui/button";
+import { AppSidebar } from "@/components/reusable/AppSidebar";
+import { Input } from "@/components/ui/input";
+export default function Dashboard() {
+  const user = useProfile();
+  return (
+    <>
+      <div className="flex flex-row min-h-screen w-full">
+        {/* Sidebar can be added here */}
+        <AppSidebar></AppSidebar>
 
-export default function Dashboard(){
-const user = useProfile()
-    return(
-        
-        <>
-        <div className="flex flex-row min-h-screen w-full">
-            
-            {/* Sidebar can be added here */}
-            <AppSidebar></AppSidebar>
-
-            <div className="ml-60 p-6 w-full">
-                <p>
-                    {user.profile?.id}
-                </p>
-                <Button> Test </Button>
-            </div>
+        <div className="ml-60 p-6 w-full">
+          <p>{user.profile?.id}</p>
+          <Button> Test </Button>
+          <Input variant={"email"}></Input>
+          <Input variant={"password"}></Input>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
